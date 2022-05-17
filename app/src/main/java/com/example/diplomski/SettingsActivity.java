1c787d764c7435cf4d55571f74c529ec470b8faf
mateoSikirca <mateo.sikirica33@gmail.com>
2022-05-17 13:37:06 +0200

    Settings finished

--- app/src/main/java/com/example/diplomski/SettingsActivity.java
+++ app/src/main/java/com/example/diplomski/SettingsActivity.java
@@ -1,5 +1,6 @@
 package com.example.diplomski;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresFeature;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,15 +9,23 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
+import com.example.diplomski.Models.Users;
 import com.example.diplomski.databinding.ActivitySettingsBinding;
 import com.google.android.gms.auth.api.signin.internal.Storage;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
+import com.squareup.picasso.Picasso;
+
+import java.util.HashMap;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -42,6 +51,41 @@ public class SettingsActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
                 startActivity(intent);
+            }
+        });
+
+        binding.saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!binding.etStatus.getText().toString().equals("") && !binding.txtUsername.getText().toString().equals("")) {
+                    String status = binding.etStatus.getText().toString();
+                    String username = binding.txtUsername.getText().toString();
+
+                    HashMap<String, Object> obj = new HashMap<>();
+                    obj.put("userName", username);
+                    obj.put("status", status);
+
+                    database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid()).updateChildren(obj);
+                    Toast.makeText(SettingsActivity.this, "Profile Updated", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SettingsActivity.this, "Please Enter Username And Status", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        database.getReference().child("Users").child(FirebaseAuth.getInstance().getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Users users = snapshot.getValue(Users.class);
+                Picasso.get().load(users.getProfilePic()).placeholder(R.drawable.avatar).into(binding.profileImage);
+
+                binding.etStatus.setText(users.getStatus());
+                binding.txtUsername.setText(users.getUserName());
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
             }
         });
 
